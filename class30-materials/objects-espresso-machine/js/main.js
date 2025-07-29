22d89543c062a6e707eee90985eb34775755ec7e
Imran Shaik <90126902+imranshahshaik@users.noreply.github.com>
2025-07-29 05:18:40 +0000

    class 30

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,21 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class Espresso{
+    constructor(color,qty,sugar,type){
+        this.color= color
+        this.qty = qty
+        this.sugar = sugar
+        this.type = type
+    }
+    make(){
+        console.log('brrrr!')
+    }
+    brew(){
+        console.log('breeeewww')
+    }
+    shake(){
+        console.log('shaaaaaaǎke')
+    }
+
+}
+
+let coffeeMachine = new Espresso('brown',2,0,'green Tea')
